934e1d2c91e518197d86cfcfce2cc9b41458dfd9
jungscom <guswjddl6176@gmail.com>
2026-03-13 12:28:45 +0900

    수정 260313

--- js/index.js
+++ js/index.js
@@ -267,4 +267,11 @@ $(function(){
 
     currentTrack = clicked[0];
   });
+
+  AOS.init({
+    duration: 500,  // 애니메이션 지속 시간 (밀리초)
+    easing: 'ease-in-out',  // 애니메이션의 이징 함수
+    delay: 100,  // 애니메이션 지연 시간 (밀리초)
+    offset: 120,  // 스크롤 감지의 시작 위치 (픽셀)
+  });
 });
